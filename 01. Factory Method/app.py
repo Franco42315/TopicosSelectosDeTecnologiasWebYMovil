e9b8004e3cf3727ea2c05917135baa6e24c026df
RafaelContrerasA <l20120970@morelia.tecnm.mx>
2024-09-11 09:11:29 -0600

    Borrar cache y cambio leve a app.py

--- 01. Factory Method/app.py
+++ 01. Factory Method/app.py
@@ -3,9 +3,9 @@ from factory.db_factory import DBFactory
 # Punto de entrada de la aplicación
 if __name__ == "__main__":
     # Definimos los datos de conexión para la base de datos MySQL
-    host = "localhost"              # El servidor de la base de datos, en este caso localhost
-    user = "root"                   # Usuario de MySQL (en XAMPP es 'root' por defecto)
-    password = ""                   # Contraseña del usuario (vacía por defecto en XAMPP)
+    host = "localhost"              # El servidor de la base de datos
+    user = "root"                   # Usuario de MySQL
+    password = "lgM57&fdRdq0"  # Contraseña 
     database = "prueba"             # Nombre de la base de datos a la que nos conectaremos
 
     # Usamos la fábrica para obtener una conexión MySQL, pasándole el tipo de base de datos y las credenciales
@@ -16,6 +16,14 @@ if __name__ == "__main__":
         # Conectamos a la base de datos y guardamos la conexión en la variable mydb
         mydb = db_connection.connect()
         print("Conexión exitosa:", mydb)  # Si la conexión es exitosa, mostramos el objeto de conexión
+
+        # Crear un cursor para ejecutar la consulta
+        mycursor = mydb.cursor()
+        mycursor.execute("SHOW TABLES")
+
+        for x in mycursor:
+            print(x)
+
     except Exception as e:
         # Si ocurre un error, mostramos el mensaje de error
         print("Error al conectar a la base de datos:", str(e))
